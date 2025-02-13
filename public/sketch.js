d76c021bd16fdb7cf0aa9d78c3b32739d2ed439f
butlerem <butlerem01@gmail.com>
2025-02-13 10:49:04 -0500

    Debugging

--- public/sketch.js
+++ public/sketch.js
@@ -7,15 +7,6 @@ function setup() {
   var c = createCanvas(1040, 550);
   c.parent('app');
 
-  // Connect to node server
-  socket = io.connect("http://localhost:3000");
-
-  // Listen for satellite data updates
-  socket.on("satelliteData", (data) => {
-    console.log("Received from server:", data);
-    satPositions = data.positions;
-  });
-  
   // Create a new gallery object.
   gallery = new Gallery();
 
@@ -25,17 +16,10 @@ function setup() {
   gallery.addVisual(new PayGapByJob2017());
   gallery.addVisual(new PayGapTimeSeries());
   gallery.addVisual(new ClimateChange());
-  gallery.addVisual(new SatelliteLocation());
-  gallery.addVisual(new CanadaSatellites());
 }
 
 function draw() {
   background(40);
-  if (gallery.selectedVisual instanceof SatelliteLocation) {
-    if (satPositions) {
-      gallery.selectedVisual.setData(satPositions);
-    }
-  }
   if (gallery.selectedVisual != null) {
     gallery.selectedVisual.draw();
   }
