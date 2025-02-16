c0322e41e0d325a524e89039e6ac8213edb0e9bb
butlerem <butlerem01@gmail.com>
2025-02-16 14:21:41 -0500

    Update sketch.js and gallery.js to accept second visualization object

--- public/gallery.js
+++ public/gallery.js
@@ -1,16 +1,13 @@
 function Gallery() {
-  
   this.visuals = [];
   this.selectedVisual = null;
+  this.selectedVisual2 = null; // New property for the second visual
   var self = this;
-
 
   // Add a new visualisation to the navigation bar.
   this.addVisual = function(vis) {
-
     // Check that the vis object has an id and name.
-    if (!vis.hasOwnProperty('id')
-        && !vis.hasOwnProperty('name')) {
+    if (!vis.hasOwnProperty('id') && !vis.hasOwnProperty('name')) {
       alert('Make sure your visualisation has an id and name!');
     }
 
@@ -19,48 +16,39 @@ function Gallery() {
       alert(`Vis '${vis.name}' has a duplicate id: '${vis.id}'`);
     }
 
-    this.visuals.push(vis);
-      
-  
+    this.visuals.push(vis); // Add the visual to the visuals array
+
     // Create menu item.
-    var menuItem = createElement('li', vis.name);
-    menuItem.addClass('menu-item');
-    menuItem.id(vis.id);
-      
-    menuItem.mouseOver(function(e)
-    {
-        
-        var el = select('#' + e.srcElement.id);
-        el.addClass("hover");
-    })
-      
-    menuItem.mouseOut(function(e)
-    {
-        var el = select('#' + e.srcElement.id);
-        el.removeClass("hover");
-    })
-      
-    menuItem.mouseClicked(function(e)
-    {
-        //remove selected class from any other menu-items
-        
-        var menuItems = selectAll('.menu-item');
-        
-        for(var i = 0; i < menuItems.length; i++)
-        {
-            menuItems[i].removeClass('selected');
-        }
-        
-        var el = select('#' + e.srcElement.id);
-        el.addClass('selected');
-        
-        self.selectVisual(e.srcElement.id);
-        
-    })
-      
-      
+    var menuItem = createElement('li', vis.name); // Create a list item with the visual's name
+    menuItem.addClass('menu-item'); // Add the 'menu-item' class to the list item
+    menuItem.id(vis.id); // Set the id of the list item to the visual's id
+
+    menuItem.mouseOver(function(e) {
+      var el = select('#' + e.srcElement.id);
+      el.addClass("hover"); // Add the 'hover' class when the mouse is over the item
+    });
+
+    menuItem.mouseOut(function(e) {
+      var el = select('#' + e.srcElement.id);
+      el.removeClass("hover"); // Remove the 'hover' class when the mouse is out of the item
+    });
+
+    menuItem.mouseClicked(function(e) {
+      // Remove selected class from any other menu-items
+      var menuItems = selectAll('.menu-item');
+      for (var i = 0; i < menuItems.length; i++) {
+        menuItems[i].removeClass('selected');
+      }
+
+      var el = select('#' + e.srcElement.id);
+      el.addClass('selected'); // Add the 'selected' class to the clicked item
+
+      // Select the visual and its related visual
+      self.selectVisual(e.srcElement.id);
+    });
+
     var visMenu = select('#visuals-menu');
-    visMenu.child(menuItem);
+    visMenu.child(menuItem); // Add the menu item to the visuals menu
 
     // Preload data if necessary.
     if (vis.hasOwnProperty('preload')) {
@@ -73,7 +61,7 @@ function Gallery() {
     // matching visId.
     for (var i = 0; i < this.visuals.length; i++) {
       if (this.visuals[i].id == visId) {
-        return i;
+        return i; // Return the index of the visual if found
       }
     }
 
@@ -81,15 +69,16 @@ function Gallery() {
     return null;
   };
 
-  this.selectVisual = function(visId){
+  this.selectVisual = function(visId) {
     var visIndex = this.findVisIndex(visId);
+    var visIndex2 = this.findVisIndex(visId + '-2'); // Assuming the second visual has an id with '-2' appended
 
     if (visIndex != null) {
       // If the current visualisation has a deselect method run it.
-      if (this.selectedVisual != null
-          && this.selectedVisual.hasOwnProperty('destroy')) {
+      if (this.selectedVisual != null && this.selectedVisual.hasOwnProperty('destroy')) {
         this.selectedVisual.destroy();
       }
+
       // Select the visualisation in the gallery.
       this.selectedVisual = this.visuals[visIndex];
 
@@ -97,10 +86,24 @@ function Gallery() {
       if (this.selectedVisual.hasOwnProperty('setup')) {
         this.selectedVisual.setup();
       }
-
-      // Enable animation in case it has been paused by the current
-      // visualisation.
-      loop();
     }
+
+    if (visIndex2 != null) {
+      // If the current second visualisation has a deselect method run it.
+      if (this.selectedVisual2 != null && this.selectedVisual2.hasOwnProperty('destroy')) {
+        this.selectedVisual2.destroy();
+      }
+
+      // Select the second visualisation in the gallery.
+      this.selectedVisual2 = this.visuals[visIndex2];
+
+      // Initialise visualisation if necessary.
+      if (this.selectedVisual2.hasOwnProperty('setup')) {
+        this.selectedVisual2.setup();
+      }
+    }
+
+    // Enable animation in case it has been paused by the current visualisation.
+    loop();
   };
 }
--- public/sketch.js
+++ public/sketch.js
@@ -1,11 +1,9 @@
 let gallery;
-const apiKey = "0ae792d9-3c7b-407a-8bf8-8fc8aea61aa1";
-const apiUrl = "https://services.nvd.nist.gov/rest/json/cves/1.0";
 
 function setup() {
   canvasContainer = select('#app');
-  var c = createCanvas(1040, 550);
-  c.parent('app');
+  var c = createCanvas(1040, 550); // Create a canvas of size 1040x550
+  c.parent('app'); // Attach the canvas to the HTML element with id 'app'
 
   // Create a new gallery object.
   gallery = new Gallery();
@@ -19,11 +17,28 @@ function setup() {
   gallery.addVisual(new EducationCompletionRate());
   gallery.addVisual(new PovertyGap());
 
+  // Add second visualisation objects here.
+  gallery.addVisual(new ClimateChange2());
+  /*gallery.addVisual(new TechDiversityRace2());
+  gallery.addVisual(new TechDiversityGender2());
+  gallery.addVisual(new PayGapByJob20172());
+  gallery.addVisual(new PayGapTimeSeries2());
+  gallery.addVisual(new EducationCompletionRate2());
+  gallery.addVisual(new PovertyGap2());*/
 }
 
 function draw() {
-  background(40);
+  background(40); // Set the background color to dark gray
   if (gallery.selectedVisual != null) {
-    gallery.selectedVisual.draw();
+    push(); // Save the current drawing state
+    translate(0, 0); // Draw the first visual on the left half
+    gallery.selectedVisual.draw(); // Call the draw method of the first visual
+    pop(); // Restore the previous drawing state
+  }
+  if (gallery.selectedVisual2 != null) {
+    push(); // Save the current drawing state
+    translate(width / 2, 0); // Draw the second visual on the right half
+    gallery.selectedVisual2.draw(); // Call the draw method of the second visual
+    pop(); // Restore the previous drawing state
   }
 }
