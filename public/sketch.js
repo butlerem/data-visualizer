1eb514665464f3103d569ce4bb928712abf56b59
butlerem <butlerem01@gmail.com>
2025-02-17 17:23:22 -0500

    Remove redundant logic

--- public/sketch.js
+++ public/sketch.js
@@ -1,13 +1,13 @@
 import { TechDiversityGender3D } from "./tech-diversity-gender-3d.js";
 
 let gallery;
-let canvasContainer; // Declare here
+let canvasContainer;
 
 // Attach p5 lifecycle functions to the window object
 window.setup = function () {
   // p5 setup
   canvasContainer = select("#canvas");
-  var c = createCanvas(1000, 550); // Create a canvas of size 1100x600
+  var c = createCanvas(1000, 550);
   c.parent("canvas"); // Attach the canvas to the HTML element with id='canvas'
 
   // Create a new gallery object
@@ -20,29 +20,13 @@ window.setup = function () {
   gallery.addVisual(new PayGapTimeSeries());
   gallery.addVisual(new ClimateChange());
   gallery.addVisual(new EducationCompletionRate());
-
-  // Add your new 3D visual
   gallery.addVisual(new TechDiversityGender3D());
 };
 
 window.draw = function () {
-  // p5 draw
-  background("#1c1c20"); // Set the background color
-
-  let gap = 50; // Space between the two visuals
-  let visualWidth = width / 2 - gap / 2; // Each visual fits within its half
+  background("#1c1c20");
 
   if (gallery.selectedVisual != null) {
-    push();
-    translate(visualWidth - width / 2, 0); // Position left visual
     gallery.selectedVisual.draw();
-    pop();
-  }
-
-  if (gallery.selectedVisual2 != null) {
-    push();
-    translate(visualWidth + gap, 0); // Position right visual
-    gallery.selectedVisual2.draw();
-    pop();
   }
 };
